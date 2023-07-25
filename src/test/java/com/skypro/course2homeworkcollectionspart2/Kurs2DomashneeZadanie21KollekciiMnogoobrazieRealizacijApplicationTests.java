5e7dc6db24650a3216c1b513529123c5ea180a46
ChernenkiyNikitych473 <nikita.chernenkiy.rus@gmail.com>
2023-07-25 10:38:01 +0200

    2 Курс ДЗ №21 Коллекции многообразие реализаций изначальный комментарий
    Signed-off-by: ChernenkiyNikitych473 <nikita.chernenkiy.rus@gmail.com>

---
+++ src/test/java/com/skypro/course2homeworkcollectionspart2/Kurs2DomashneeZadanie21KollekciiMnogoobrazieRealizacijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.course2homeworkcollectionspart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs2DomashneeZadanie21KollekciiMnogoobrazieRealizacijApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
